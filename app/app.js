a6a1cd60131d077131c15326c18087dce6693e41
nikhilsm930 <nikhilsm930@gmail.com>
2024-03-20 13:10:25 +0530

    latest@20/03/2024

--- app/app.js
+++ app/app.js
@@ -7,15 +7,6 @@ const orderRouter = require('../router/order/orderRouter');
 const app=express();
 const local=0
 let baseUrl;
-
-//middleware
-app.use(morgan('dev'));
-//Routes Folder
-// Product Endpoints
-// console.log(local);
-// console.log(process.env.BASEURL_PROD);
-// console.log(process.env.BASEURL_DEV);
-console.log(typeof(local))
 if(local){
     baseUrl=process.env.BASEURL_PROD;
 }
@@ -23,8 +14,11 @@ else{
     baseUrl=process.env.BASEURL_DEV;
     
 }
-console.log(local);
-console.log(baseUrl)
+
+//middleware
+app.use(morgan('dev'));
+//Routes Folder
+// Product Endpoints
 app.use(`${baseUrl}products`,productRouter);
 /// User Endpoints
 app.use(`${baseUrl}users`, userRouter);
